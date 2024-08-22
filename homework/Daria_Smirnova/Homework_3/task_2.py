dac8b31c5cebd5eed9e6c12fc964be36f4d81a34
Daria256 <kalikina.daria@yandex.ru>
2024-08-22 12:25:06 +0200

    Homework_3 has been fixed with syntax errors

--- homework/Daria_Smirnova/Homework_3/task_2.py
+++ homework/Daria_Smirnova/Homework_3/task_2.py
@@ -1,4 +1,3 @@
 x = 100
 y = 20
-print ((x-y)/(1+x*y))
-
+print((x - y) / (1 + x * y))
--- homework/Daria_Smirnova/Homework_3/task_3.py
+++ homework/Daria_Smirnova/Homework_3/task_3.py
@@ -1,4 +1,4 @@
 a = 190
 b = 150
-print ((a*b) ** 0.5)
-print ((a+b)/2)
+print((a * b) ** 0.5)
+print((a + b) / 2)
