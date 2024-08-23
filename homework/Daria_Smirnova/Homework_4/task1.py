b6d44085d3103850335373c8f9a679c9d02adeee
Daria256 <kalikina.daria@yandex.ru>
2024-08-23 12:42:22 +0200

    fixed for tuple

--- homework/Daria_Smirnova/Homework_4/task1.py
+++ homework/Daria_Smirnova/Homework_4/task1.py
@@ -10,7 +10,9 @@ my_dict["list"].append("Vasy")
 my_dict["list"].pop(1)
 
 del my_dict["dict"]["one"]
-my_dict["dict"]['i am tuple'] = 1234567890
+my_dict ["dict"][('i am a tuple',)] = 123456
+
+
 
 my_dict["set"].add(6)
 my_dict["set"].remove(1)
