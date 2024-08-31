eab9d60ab3e9fb5d09fcbc76054132d4a0de1772
Daria256 <kalikina.daria@yandex.ru>
2024-08-31 19:40:04 +0200

    Homework_6 is done

---
+++ homework/Daria_Smirnova/homework_6/task1.py
@@ -0,0 +1,9 @@
+text = "Etiam tincidunt neque erat, quis molestie enim imperdiet vel. Integer urna nisl, facilisis vitae semper at, dignissim vitae libero"
+words = text.split()
+
+for word in words:
+    if word[-1] == "," or word[-1] == ".":
+        modified_word = word[:-1] + "ing" + word[-1]
+    else:
+        modified_word = word + "ing"
+    print(modified_word, end=" ")
