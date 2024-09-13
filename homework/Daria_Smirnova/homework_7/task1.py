5e2fe7bb29af162268cfb0c7d838278937b75f45
Daria256 <kalikina.daria@yandex.ru>
2024-09-13 16:55:37 +0200

    homework_7 is done

---
+++ homework/Daria_Smirnova/homework_7/task1.py
@@ -0,0 +1,9 @@
+number = 10
+
+while True:
+    a = int(input("Enter a number: "))
+    if a == number:
+        print("Поздравляю! Вы угадали")
+        break
+    else:
+        print("попробуйте снова")
