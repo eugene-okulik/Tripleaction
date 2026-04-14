857e4e6ae82342540c7feb327bf350c1cf777a9b
Daria256 <kalikina.daria@yandex.ru>
2026-04-14 19:24:04 +0200

    homework_25 is done

---
+++ homework/Daria_Smirnova/homework_25/task1.py
@@ -0,0 +1,15 @@
+from selenium import webdriver
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome()
+wait = WebDriverWait(driver, 5)
+driver.get("https://www.qa-practice.com/elements/input/simple")
+input_text = "tester"
+
+text_string = wait.until(EC.element_to_be_clickable((By.ID, 'id_text_string')))
+text_string.send_keys(input_text)
+text_string.submit()
+entered_text = driver.find_element(By.XPATH, "//*[contains(text(), 'tester')]")
+print(entered_text.text)
